2e602b1955a603adac4fa969f3133e46a2585354
bjanssens <bjanssens@inuits.eu>
2014-08-06 16:06:39 +0200

    Updated the manifest
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/nodes/fmp01.pp
+++ manifests/nodes/fmp01.pp
@@ -3,6 +3,8 @@ node 'fmp01' {
 
   # Place puppet stuff here
 
+  $soft=['rpm-build','ruby-devel','yum-downloadonly']
+
   # Adding epel repo
   yumrepo { 'epel':
     name       => 'epel',
@@ -12,4 +14,33 @@ node 'fmp01' {
     gpgcheck   => 0,
   }
 
+# Add the inuits repos for looking for packages
+# Dont forget the ssh tunnel
+#
+#   Yumrepo{
+#     gpgcheck => '0',
+#     enabled  => '1',
+#   }
+#   
+#   yumrepo{'inuits':
+#     descr   => 'CentOS-6 - Inuits',
+#     baseurl => 'http://pulp2.internal.inuits.eu/pulp/repos/pub/inuits/$releasever/$basearch',
+#   }
+#   
+#   yumrepo{'inuits-private':
+#     baseurl => 'http://pulp2.internal.inuits.eu/pulp/repos/private/inuits-private',
+#     descr   => 'Inuits internal repo',
+#   }
+  
+  package { $soft:
+    ensure   => 'present',
+    require  => Yumrepo['epel'],
+  }
+
+  # Get the fpm gem
+  exec { 'fpm-gem':
+    command => '/usr/bin/gem install fpm',
+    require => Package['ruby-devel'],
+    creates => '/usr/lib/ruby/gems/1.8/doc/fpm-1.1.0/rdoc/index.html'
+  }
 }
